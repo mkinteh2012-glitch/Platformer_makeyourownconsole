65f7c206c6d9412f59caa236244954d79683827e
mkinteh2012-glitch <mkinteh2012@gmail.com>
2026-01-09 22:23:38 -0800

    added player animation

--- Game/Script/killzone.gd
+++ Game/Script/killzone.gd
@@ -5,10 +5,13 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void: # if touching kill area
 	pass # Replace with function body.
 	print("you died") # you died in terminal
+	Engine.time_scale = 0.5 #slows down the game to 50%
+	body.get_node("CollisionShape2D").queue_free() #Finds collison shape in player and disables it
 	timer.start() #Death cooldown
 	
 
 
 func _on_timer_timeout() -> void: #after timer's over
 	pass # Replace with function body.
+	Engine.time_scale = 1 # sets speed to normal
 	get_tree().reload_current_scene() # reset the player
--- Game/Script/slime.gd
+++ Game/Script/slime.gd
@@ -6,8 +6,15 @@ var direction = 1 #Enemy direction
 
 @onready var ray_cast_right: RayCast2D = $RayCastRight # making raycast variables
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite:AnimatedSprite2D = $AnimatedSprite2D #animated sprite node
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	if ray_cast_right.is_colliding(): # if the raycast right touches a wall
+			direction = -1 #turn around
+			animated_sprite.flip_h = true #flips the enemy
+	if ray_cast_left.is_colliding():# check raycast right touches a walll
+			direction = 1 #turn around
+			animated_sprite.flip_h = false
 	position.x += direction * SPEED * delta
